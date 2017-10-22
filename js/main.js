57951ca68bcf52712a6695f27058f573a5645553
boxing <boxingnguyen@gmail.com>
2017-10-22 03:13:39 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	$('.controller').mousedown(function() {
+        console.log('a');
+		var direction = $(this).text();
+		$.ajax ({
+			url: 'controller.php',
+			type: 'post',
+			dataType: "text",
+			data: { move : direction},
+			success: function(data) {
+				if (data) {
+					console.log('move ' + direction);
+				} else {
+					return "the deo nao";
+				}
+			},
+			error: function(error) {
+		       alert ("Error: " + error.statusText);
+		    }
+		});
+	});
+	$('.controller').mouseup(function () {
+		var direction = $(this).text();
+		$.ajax ({
+			url: 'reset.php',
+			type: 'post',
+			dataType: "text",
+			data: { reset : direction},
+			success: function(nice) {
+				if (nice) {
+					console.log('reset ' + direction);
+				} else {
+					return "the deo nao";
+				}
+			},
+			error: function(error) {
+		       alert ("Error: " + error.statusText);
+		    }
+		});	
+	});
+});
+
